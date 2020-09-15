50e6c07f0e49be4320ddf89978912b030f14b50b
Marek Szweda <mszweda@kbj.com.pl>
2020-09-15 16:41:12 +0200

    Jak zrobić Client Side Load Balancing - Ribbon.
    Tu na sztywno definiuję dostępne serwery, które mogą być odpytywane
    w listOfServers.
    W parametrze listOfServers wartości podaje się bez spacji
    dzielone przecinkiem.
    
    WAŻNE:
    pingowanie dostępnych serwisów odbywa się przez adres "/"
    (puste uri).

--- Service/src/main/java/service/Service.java
+++ Service/src/main/java/service/Service.java
@@ -5,14 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
 @SpringBootApplication
 @RestController
+@RibbonClient(name = "simple-api", configuration = SimpleApiConfiguration.class)
 public class Service {
-
+  @LoadBalanced
   @Bean
   RestTemplate restTemplate(){
     return new RestTemplate();
@@ -23,7 +26,7 @@ public class Service {
 
   @RequestMapping("/service")
   public String service() {
-    String out = this.restTemplate.getForObject("http://localhost:8090/simple", String.class);
+    String out = this.restTemplate.getForObject("http://simple-api/simple", String.class);
     return out;
   }
 
